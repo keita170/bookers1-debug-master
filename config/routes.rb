f2858239fa9c9f16b112aadc9c9311417c745c9b
keita170 <noinoi0565@gmail.com>
2021-07-22 23:44:08 +0000

    finish™

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
-  resources :books, only: [:show, :index, :edit, :create]
-  
+  resources :books, only: [:show, :index, :edit, :create, :update]
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
 end
